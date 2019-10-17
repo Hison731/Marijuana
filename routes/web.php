eda16048e8dee01ddb293c89c2c5f3b01055494d
hison731 <jinxing4@outlook.com>
2019-10-16 18:12:03 -0700

    vue components, add/view api

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('post');
+})->where('any', '.*');
